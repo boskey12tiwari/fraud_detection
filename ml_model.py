e5dbc66d4dcdb4c4b942192ee80f1d475eb31ea0
boskey12tiwari <boskey12tiwari@gmail.com>
2022-03-24 12:14:35 +0530

    added 3 new ml model

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,12 @@
 print("adding model related code here")
+
+
+
+def model1():
+    print("adding code for ML Model1")
+
+def model2():
+    print("adding code for ML Model2")
+
+def model3():
+    print("adding code for ML Mode3")
